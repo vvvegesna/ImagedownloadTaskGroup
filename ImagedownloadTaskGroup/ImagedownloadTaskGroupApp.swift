77339c341d6a86c3a152d38fcdb023035530a510
Vegesna, Vijay Varma <vijayv.vegesna@iqvia.com>
2022-07-23 05:44:37 -0700

    Initial Commit

---
+++ ImagedownloadTaskGroup/ImagedownloadTaskGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagedownloadTaskGroupApp.swift
+//  ImagedownloadTaskGroup
+//
+//  Created by Vegesna, Vijay Varma on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ImagedownloadTaskGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
